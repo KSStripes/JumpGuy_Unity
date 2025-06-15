5a407810eeaaf15181a196f5d75c14a795d708cc
KSStripes <kristin.schumann@gmail.com>
2025-06-15 18:01:44 +0200

    Implement Movecontroller

---
+++ Assets/Scripts/MoveController.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveController : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+
+    private Rigidbody2D rb;
+    private Animator animator;
+    private SpriteRenderer spriteRenderer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float move = Input.GetAxisRaw("Horizontal");
+        rb.velocity = new Vector2(move * moveSpeed, rb.velocity.y);
+
+        //Flip sprite when direction changes
+        if (move < 0) spriteRenderer.flipX = true;
+        else if (move > 0) spriteRenderer.flipX = false;
+
+        // Update animation
+        bool isRunning = Mathf.Abs(move) > 0.01f;
+        animator.SetBool("isRunning", isRunning);
+    }
+}
