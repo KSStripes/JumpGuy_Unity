80abd2f8dfbaf152082ab7dedb8d1503f66238a2
KSStripes <kristin.schumann@gmail.com>
2025-06-22 14:17:00 +0200

    add coin.cs and coin collecting audio

---
+++ Assets/Scripts/Coin.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour
+{
+    private AudioSource audioSource;
+    private bool collected = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>(); //at start get sound
+    }
+
+    // Trigger : when collected
+void OnTriggerEnter2D(Collider2D other)
+    {
+        if (collected) return; // Prevent double collection
+
+        if (other.CompareTag("Player"))
+        {
+            collected = true;
+
+            // Play coin sound
+            if (audioSource != null) audioSource.Play();
+
+            // Notify player’s CoinCollector (if present)
+            CoinCollector collector = other.GetComponent<CoinCollector>();
+            if (collector != null) collector.IncCoinCount();
+
+            // Hide coin visuals
+            GetComponent<SpriteRenderer>().enabled = false;
+            GetComponent<Collider2D>().enabled = false;
+
+            // Delay destruction to allow sound to finish
+            Destroy(gameObject, 0.3f);
+        }
+    }
+}
--- Assets/Scripts/CoinCollector.cs
+++ Assets/Scripts/CoinCollector.cs
@@ -6,16 +6,10 @@ public class CoinCollector : MonoBehaviour
 {
     private int coinCount = 0; // Tracks number of coins collected
 
-
-    // called when this coin enters a trigger collider
-    void OnTriggerEnter2D(Collider2D other)
+    // increment a coin count, which will be referenced in coin.cs, when item is picked up
+    public void IncCoinCount()
     {
-        // check for collectable tag
-        if (other.CompareTag("Collectable"))
-        {
-            Destroy(other.gameObject); // delete the coin from screen
-            coinCount++; //increase coinCount by 1
-            Debug.Log("Collected Coins: " + coinCount);
-        }
+        coinCount++;
+        Debug.Log("Collected Coins: " + coinCount);
     }
 }
