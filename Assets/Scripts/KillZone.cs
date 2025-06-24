a869bcf1d7030cdd9692ba5e07c9bd6d3d77c8f4
KSStripes <kristin.schumann@gmail.com>
2025-06-25 00:17:44 +0200

    Added Falling Sound

--- Assets/Scripts/KillZone.cs
+++ Assets/Scripts/KillZone.cs
@@ -1,11 +1,25 @@
 using UnityEngine;
+using UnityEngine.Video;
 
 public class KillZone : MonoBehaviour
 {
+    private AudioSource audioSource;
+
+    void Start()
+    {
+        // play falling sound
+        audioSource = GetComponent<AudioSource>(); 
+    }
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
+
+            if (audioSource != null)
+            {
+                audioSource.Play();
+            }
             GameManager.Instance?.LoseLife();
         }
     }
