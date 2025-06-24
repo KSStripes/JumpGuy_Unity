f4d4accdd0739f554689ec3dddd5d689a76a6c23
KSStripes <kristin.schumann@gmail.com>
2025-06-24 13:14:55 +0200

    Added GroundCheck to Player for jumping

--- Assets/Scripts/JumpController.cs
+++ Assets/Scripts/JumpController.cs
@@ -1,55 +1,41 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class JumpController : MonoBehaviour
 {
     public float jumpForce = 12f;
+    public Transform groundCheck; // assign in Inspector
+    public float groundCheckRadius = 0.5f;
+    public LayerMask groundLayer; // set this in Inspector to "Ground"
+
     private Rigidbody2D rb;
     private bool isGrounded;
     private Animator animator;
 
-    // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        // Check for jump input and char on ground
+        // Ground detection using overlap circle
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
+
         if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
         {
-            //Debug.Log("attempt jump");
-            // Adjust velocity with a vector of the jumpForce and the current velocity
             rb.velocity = new Vector2(rb.velocity.x, jumpForce);
         }
+
+        animator.SetBool("isGrounded", isGrounded);
     }
 
-    // Check if char is back on ground, update boolean isGrounded
-    void OnCollisionEnter2D(Collision2D collision)
+    void OnDrawGizmosSelected()
     {
-        Debug.Log("Collided with: " + collision.gameObject.name);
-
-        // Detect ground contact
-        if (collision.gameObject.CompareTag("Ground"))
+        if (groundCheck != null)
         {
-            isGrounded = true;
-            //Debug.Log("player is grounded");
-            animator.SetBool("isGrounded", true);
-        }
-    }
-
-    // Check if char is in air, update boolean isGrounded
-    void OnCollisionExit2D(Collision2D collision)
-    {
-        if (collision.gameObject.CompareTag("Ground"))
-        {
-            isGrounded = false;
-            //Debug.Log("player is off ground");
-            animator.SetBool("isGrounded", false);
+            Gizmos.color = Color.red;
+            Gizmos.DrawWireSphere(groundCheck.position, groundCheckRadius);
         }
     }
 }
